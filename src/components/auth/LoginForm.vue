ba52f305fc799c6edad0ae889867dd100f7c34ac
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-04-21 23:03:54 +0200

    loading improve

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -5,14 +5,18 @@
 				<h1 class="text-uppercase pull-center text-primary">Connexion</h1>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.email }}</p>
-					<input v-focus v-model="input.email" type="email" data-type="email" name="email" id="email" class="form-control input-lg w-100" placeholder="E-mail">
+					<input v-focus v-model="input.email" type="email" data-type="email" name="email" id="email" class="form-control input-lg w-100" placeholder="E-mail" :disabled="loading">
 				</div>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.password }}</p>
-					<input v-model="input.password" type="password" data-type="password" name="password" id="password" class="form-control input-lg w-100" placeholder="Mot de passe">
+					<input v-model="input.password" type="password" data-type="password" name="password" id="password" class="form-control input-lg w-100" placeholder="Mot de passe" :disabled="loading">
 				</div>
 				<div>
-					<button type="submit" class="btn btn-lg btn-primary text-uppercase w-100"><i class="fas fa-sign-in-alt"></i> Se connecter</button>
+					<button type="submit" class="btn btn-lg btn-primary text-uppercase w-100 d-flex align-items-center p-0" :disabled="loading">
+						<span class="iSubmit" v-if="loading"><img src="../../assets/spinner.gif" height="20"></span>
+						<span class="iSubmit" v-else><i class="fas fa-sign-in-alt"></i></span>
+						<span class="flex-grow-1">{{ loadingMsg }}</span>
+					</button>
 				</div>
 			</fieldset>
 		</form>
@@ -28,6 +32,8 @@ export default {
 	name: 'LoginForm',
 	data () {
 		return {
+			loading: false,
+			loadingMsg: 'Se connecter',
 			input: {
 				email: '',
 				password: ''
@@ -41,7 +47,7 @@ export default {
 	mixins: [checkForm, login],
 	methods: {
 		login: function() {
-			this.$loading.show({delay: 0});
+			this.loading = true; this.loadingMsg = '...connection';
 			const inputs = document.getElementById("loginForm").querySelectorAll("input");
 			const goSignup = this.checkForm(inputs);
 			for (let element in this.errorMsg) {
@@ -53,18 +59,28 @@ export default {
 					"password": this.input.password
 				})
 				.then(response => {
-					EventBus.$emit('logged', { message: response.data });
-					this.$router.push('Home');
+					setTimeout( () => {
+						this.$router.push({ path: '/home'});
+						EventBus.$emit('logged', { message: response.data });
+					}, 1500);
+					//this.$router.push('Home');
 				})
 				.catch(error => {
 					localStorage.clear();
 					this.errorMsg[error.response.data.objet] = error.response.data.message;
+					this.loading = false; this.loadingMsg = 'Se connecter';
 				});
 			} else {
 				this.errorMsg.email = 'Veuillez vérifier vos informations.';
+				this.loading = false; this.loadingMsg = 'Se connecter';
 			}
-			this.$loading.hide();
 		}
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.iSubmit {
+	padding: 0.375rem 0.75rem;
+}
+</style>
--- src/components/auth/SignupForm.vue
+++ src/components/auth/SignupForm.vue
@@ -6,24 +6,28 @@
 				<p class="alerte text-danger">{{ errorMsg.general }} </p>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.firstName }}</p>
-					<input v-focus v-model="input.firstName" type="text" data-type="text" name="firstName" id="firstName" class="form-control input-lg w-100" placeholder="Prénom" required>
+					<input v-focus v-model="input.firstName" type="text" data-type="text" name="firstName" id="firstName" class="form-control input-lg w-100" placeholder="Prénom" required :disabled="loading">
 				</div>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.lastName }}</p>
-					<input v-model="input.lastName" type="text" data-type="text" name="lastName" id="lastName" class="form-control input-lg w-100" placeholder="Nom" required>
+					<input v-model="input.lastName" type="text" data-type="text" name="lastName" id="lastName" class="form-control input-lg w-100" placeholder="Nom" required :disabled="loading">
 				</div>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.email }}</p>
-					<input v-model="input.email" type="email" data-type="email" name="email" id="email" class="form-control input-lg w-100" placeholder="E-mail" required>
+					<input v-model="input.email" type="email" data-type="email" name="email" id="email" class="form-control input-lg w-100" placeholder="E-mail" required :disabled="loading">
 				</div>
 				<div class="form-group">
 					<p class="text-left alerte">{{ errorMsg.password }}</p>
-					<input v-model="input.password" type="password" data-type="password" name="password" id="password" class="form-control input-lg invalid w-100" placeholder="Mot de passe">
+					<input v-model="input.password" type="password" data-type="password" name="password" id="password" class="form-control input-lg invalid w-100" placeholder="Mot de passe" :disabled="loading">
 					<br>
 					<CheckPwd />
 				</div>
 				<div>
-					<button type="submit" class="btn btn-lg btn-primary-2 text-uppercase w-100">S'inscrire</button>
+					<button type="submit" class="btn btn-lg btn-primary-2 text-uppercase w-100 d-flex align-items-center p-0" :disabled="loading">
+						<span class="iSubmit" v-if="loading"><img src="../../assets/spinner.gif" height="20"></span>
+						<span class="iSubmit" v-else><i class="fas fa-sign-in-alt"></i></span>
+						<span class="flex-grow-1">{{ loadingMsg }}</span>
+					</button>
 				</div>
 			</fieldset>
 		</form>
@@ -40,6 +44,8 @@ export default {
 	name: 'SignupForm',
 	data () {
 		return {
+			loading: false,
+			loadingMsg: 'S\'inscrire',
 			input: {
 				firstName: '',
 				lastName: '',
@@ -83,7 +89,7 @@ export default {
 	mixins: [checkForm, login],
 	methods: {
 		signup: function() {
-			this.$loading.show({delay: 0});
+			this.loading = true; this.loadingMsg = '...inscription';
 			const inputs = document.getElementById("signupForm").querySelectorAll("input");
 			const goSignup = this.checkForm(inputs);
 			for (let element in this.errorMsg) {
@@ -95,28 +101,34 @@ export default {
 					user: JSON.stringify(user)
 				})
 				.then(() => {
+					this.loadingMsg = '...connexion';
 					this.afterLogin({
 						"email": user.email,
 						"password": user.password
 					})
 					.then(response => {
-						EventBus.$emit('logged', { message: response.data });
-						this.$router.push('Home');
+						setTimeout( () => {
+							this.$router.push({ path: '/home'});
+							EventBus.$emit('logged', { message: response.data });
+						}, 1500);
 					})
 					.catch(error => {
 						localStorage.clear();
 						this.errorMsg.firstName = error.response.data.message;
+						this.loading = false; this.loadingMsg = 'S\'inscrire';
 					});
 				})
 				.catch(error => {
 					this.errorMsg.firstName = error.response.data.message;
+					this.loading = false; this.loadingMsg = 'S\'inscrire';
 				});
 			} else {
 				for (const property in goSignup.elements) {
 					this.errorMsg[property] = goSignup.elements[property];
+					this.loading = false; this.loadingMsg = 'S\'inscrire';
 				}
 			}
-			this.$loading.hide();
+			this.loading = false; this.loadingMsg = 'S\'inscrire';
 		}
     },
 	components: {
@@ -144,4 +156,8 @@ input {
 		opacity: 1;
 	}
 };
+
+.iSubmit {
+	padding: 0.375rem 0.75rem;
+}
 </style>
